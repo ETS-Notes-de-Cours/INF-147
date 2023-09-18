0aa00950a2fd61bf801bf56bf21e452ddd80f160
Colin Boulé <124721545+Colb54@users.noreply.github.com>
2023-09-18 09:46:08 -0400

    Update exercice_de_base1.c

--- Devoirs/exercice_de_base1.c
+++ Devoirs/exercice_de_base1.c
@@ -6,6 +6,21 @@ int exercice = 1;
 int main() {
     switch(exercice){
       case 1 :
+        int nombreDeBoites,qtyArticle=36;;
+        float poidsBoite, poidsTotal, poidsArticle;
+
+        printf("\n\rVeuillez saisir le poids d'un article (en kilogrammes) : ");
+        scanf("%f", &poidsArticle);
+
+        printf("\n\rVeuillez saisir le nombre de boîtes : ");
+        scanf("%d", &nombreDeBoites);
+
+        poidsBoite = poidsArticle*qtyArticle;
+
+        poidsTotal = poidsBoite * nombreDeBoites;
+
+        printf("\n\rLe poids d'une boite d'articles est de %.2lf kilogrammes.\n", poidsBoite);
+        printf("\n\rLe poids total de %d boites est de %.2lf kilogrammes.\n", nombreDeBoites, poidsTotal);
         break;
       
       case 2 :
