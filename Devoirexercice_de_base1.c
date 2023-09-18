adbfd43e47136912e13f62770be61295dd202417
Colin Boulé <124721545+Colb54@users.noreply.github.com>
2023-09-18 09:34:19 -0400

    Create Devoirexercice_de_base1.c

---
+++ Devoirexercice_de_base1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int exercice = 1;
+
+int main() {
+    switch(exercice){
+      case 1 :
+        break;
+      
+      case 2 :
+        break;
+      
+      case 3 :
+        break;
+      
+      case 4 :
+        break;
+      
+      case 5 :
+        break;
+      
+      case 6 :
+        break;
+      
+      case 7 :
+        break;
+      
+      case 8 :
+        break;
+      
+      case 9 :
+        break;
+      
+      case 10 :
+        break;
+      
+    }
+}
